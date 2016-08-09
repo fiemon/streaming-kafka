34cf0d15b4305cb7214b85fae3b5b473436b7fe9
山野祐 <yuyamano1206@gmail.com>
2016-08-09 16:51:46 +0900

    kafka→spark連携確認のため

--- src/main/scala/org/littlewings/spark/KafkaStreaming.scala
+++ src/main/scala/org/littlewings/spark/KafkaStreaming.scala
@@ -2,11 +2,11 @@ package org.littlewings.spark
 
 import java.io.StringReader
 import org.apache.spark.SparkContext._
+import org.apache.spark.streaming.kafka._
 import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.rdd.RDD._
 import org.apache.spark.streaming.{Durations, StreamingContext}
-//import org.apache.spark.streaming.kafka._
 import java.util.Properties
 import org.apache.spark._
 
@@ -17,8 +17,8 @@ object KafkaStreaming {
 
     println("\n====================== Start. ======================")
     //    logger.debug(s"Some message!")
-//    val config = new java.util.Properties
-//    config.load(this.getClass().getClassLoader().getResourceAsStream("config.properties"))
+    val config = new java.util.Properties
+    config.load(this.getClass().getClassLoader().getResourceAsStream("config.properties"))
 
 //    System.setProperty("twitter4j.oauth.consumerKey", config.get("oauth.consumerKey").toString)
 //    System.setProperty("twitter4j.oauth.consumerSecret", config.get("oauth.consumerSecret").toString)
@@ -32,10 +32,11 @@ object KafkaStreaming {
 
     println("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
     // kafka
-    val kafkaStream = KafkaUtils.createStream(ssc, "devblog-kafka:2181", "default", Map("syslog-topic" -> 1))
-    val kafkaInputCount = kafkaStream.count()
+//    val stream = KafkaUtils.createStream(ssc, "devblog-kafka:2181", "console-consumer", Map("syslog-topic" -> 1)
+    val stream = KafkaUtils.createStream(ssc, "devblog-kafka:2181", "0", Map("syslog-topic" -> 1))
+    val kafkaInputCount = stream.count()
     kafkaInputCount.print
-    println("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb")
+
 //    val lines = ssc.socketTextStream("devblog-kafka", 9999)
 //    val words = lines.flatMap(_.split(" "))
 //    val pairs = words.map(word => (word, 1))
@@ -64,42 +65,44 @@ object KafkaStreaming {
 //          tokenStream.end()
 //        }
 //      }
-//
+
 //    // ２桁以上の文字を対象にアルファベット、数値のみはngwordという単語とする
-//    val wordAndOnePairRDD = tweetRDD.map(word => (ngwordConvert(word)))
+//    val wordAndOnePairRDD = stream.map(word => (ngwordConvert(word)))
 //    // (Apache, 1) (Spark, 1) というペアにします。ngwordはゴミ単語なので0を設定
 //    val ngwordRDD = wordAndOnePairRDD.map(word => (word, word match {
 //      case ("ngword") => 0
 //      case _ => 1
 //    }))
-
-    // countup reduceByKey(_ + _) は　reduceByKey((x, y) => x + y) と等価です。
+//
+//    // countup reduceByKey(_ + _) は　reduceByKey((x, y) => x + y) と等価です。
 //    val wordAndCountRDD = ngwordRDD.reduceByKey((a, b) => a + b)
-    //    val wordAndCountRDD = wordAndOnePairRDD.reduceByKey(_ + _)
-
-    // key => value value => keyに変更
+////    val wordAndCountRDD = wordAndOnePairRDD.reduceByKey(_ + _)
+//
+//    // key => value value => keyに変更
 //    val countAndWordRDD = wordAndCountRDD.map { wordAndWount => (wordAndWount._2, wordAndWount._1) }
-
-    // sort transformをかまさないとsortByKeyが使えない
+//
+//    // sort transformをかまさないとsortByKeyが使えない
 //    val sortedCWRDD = countAndWordRDD.transform(rdd => rdd.sortByKey(false))
-
-    // value => key key => valueに変更
+//
+//    // value => key key => valueに変更
 //    val sortedCountAndWordRDD = sortedCWRDD.map { countAndWord => (countAndWord._2, countAndWord._1) }
 
     // データ保存先をconfigから取得してdevとliveで保存先切り替える
 //    sortedCountAndWordRDD.saveAsTextFiles(config.get("save.file.dir").toString + args(1))
+    stream.print()
+//    stream.saveAsTextFiles(config.get("save.file.dir").toString + args(1))
     //    sortedCountAndWordRDD.print()
 
     // streaming start
-//    ssc.start()
-//    ssc.awaitTermination()
+    ssc.start()
+    ssc.awaitTermination()
   }
 
-//  def PartOfSpeechCheckConvert(part: String, word: String) = {
-//    if (part.split("-")(0) == "名詞") word else "ngword"
-//  }
+  def PartOfSpeechCheckConvert(part: String, word: String) = {
+    if (part.split("-")(0) == "名詞") word else "ngword"
+  }
 
-//  def ngwordConvert(word: String) = {
-//    if (word.length >= 2) word.replaceAll("(^[a-z]+$)", "ngword").replaceAll("^[0-9]+$", "ngword") else "ngword"
-//  }
+  def ngwordConvert(word: String) = {
+    if (word.length >= 2) word.replaceAll("(^[a-z]+$)", "ngword").replaceAll("^[0-9]+$", "ngword") else "ngword"
+  }
 }
